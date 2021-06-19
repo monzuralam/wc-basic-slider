6bd9d84ce295e70c5179fdb01261460f7572bad2
monzuralam <monzur707@gmail.com>
2021-06-19 23:39:51 +0600

    secound slider init

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,32 @@ var swiper = new Swiper(".firstCarousel", {
         },
     },
 });
+
+/**
+ * Secound Slider
+ */
+ var swiper = new Swiper(".secoundCarousel", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    loop:true,
+    autoplay:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+    },
+});
