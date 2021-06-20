53853832d01b3623d340375b722e4cff54b11963
monzuralam <monzur707@gmail.com>
2021-06-21 00:25:19 +0600

    third carousel init

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,3 +52,32 @@ var swiper = new Swiper(".firstCarousel", {
         },
     },
 });
+
+/**
+ * Third Slider
+ */
+ var swiper = new Swiper(".thirdCarousel", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    loop:true,
+    autoplay:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+    },
+});
