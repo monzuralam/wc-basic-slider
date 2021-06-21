9695c6a711673a8206a1ab3513074f7f5836d84c
monzuralam <monzur707@gmail.com>
2021-06-21 23:22:29 +0600

    four carousel init

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,3 +81,37 @@ var swiper = new Swiper(".firstCarousel", {
         },
     },
 });
+
+
+/**
+ * Third Slider
+ */
+ var swiper = new Swiper(".fourCarousel", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    loop:true,
+    autoplay:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+    },
+});
