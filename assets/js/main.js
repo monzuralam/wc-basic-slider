667d7b54d9f9ff6f3fbdd348de3a5cd7906075e0
monzuralam <monzur707@gmail.com>
2021-06-22 22:30:14 +0600

    fifth slider init

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,9 +84,42 @@ var swiper = new Swiper(".firstCarousel", {
 
 
 /**
- * Third Slider
+ * Four Slider
  */
  var swiper = new Swiper(".fourCarousel", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    loop:true,
+    autoplay:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+    },
+});
+
+/**
+ * Fifth Slider
+ */
+ var swiper = new Swiper(".fiveCarousel", {
     slidesPerView: 4,
     spaceBetween: 10,
     loop:true,
